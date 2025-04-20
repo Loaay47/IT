6bae152cbde6fcb9f02cc2ced51bc5f84ff851a9
Abdelrahman <abdelrahman0ahmed1@gmail.com>
2025-04-20 21:08:27 +0200

    JS validation

--- js/validation.js
+++ js/validation.js
@@ -1,41 +1,121 @@
-function validateForm(event) {
+function validateLogin(event) {
     event.preventDefault();
-
-    let valid = true;
 
     const email = document.forms["userForm"]["email"].value;
     const password = document.forms["userForm"]["password"].value;
+    const messageBox = document.getElementById("loginMessage");
+    const errorBox = document.getElementById("ErrorMessage");
 
     if (!validateEmail(email)) {
-        alert("Please enter a valid email address.");
-        valid = false;
+        errorBox.textContent = "Invalid Email.";
+        errorBox.style.display = "block";
+
+        setTimeout(() => {
+            errorBox.style.display = "none";
+        }, 1500);
+        return false;
     }
 
     if (password.length < 6) {
-        alert("Password must be at least 6 characters.");
-        valid = false;
-    }
+        errorBox.textContent = "Short Password.";
+        errorBox.style.display = "block";
 
-    if (valid) {
-        alert("Form is valid!");
-        document.forms["userForm"].submit();
+        setTimeout(() => {
+            errorBox.style.display = "none";
+        }, 1500);
+        return false;
     }
+    messageBox.textContent = "Login Succesful! Redirecting...";
+    messageBox.style.display = "block";
 
-    return valid;
+    localStorage.setItem("isLoggedIn", "true");
+    setTimeout(() => {
+        window.location.href = "index.html";
+    }, 1500);
 
-    function validateEmail(email) {
-        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return regex.test(email);
-    }
-
-    function switchTheme(themeName) {
-        const theme = document.body;
-        if (themeName === 'dark') {
-            theme.classList.remove('light-theme');
-            theme.classList.add('dark-theme');
-        } else {
-            theme.classList.remove('dark-theme');
-            theme.classList.add('light-theme');
-        }
-    }
+    return false;
 }
+function validateRegister(event) {
+    event.preventDefault();
+
+    const username = document.forms["registerForm"]["username"].value;
+    const email = document.forms["registerForm"]["email"].value;
+    const password = document.forms["registerForm"]["password"].value;
+    const confirm_pass = document.forms["registerForm"]["confirmPassword"].value;
+    const messageBox = document.getElementById("loginMessage");
+    const errorBox = document.getElementById("ErrorMessage");
+
+    if (username.length < 6) {
+        errorBox.textContent = "Short username. (Min 6 letters)";
+        errorBox.style.display = "block";
+
+        setTimeout(() => {
+            errorBox.style.display = "none";
+        }, 1500);
+        return false;
+    }
+
+    if (!validateEmail(email)) {
+        errorBox.textContent = "Invalid Email.";
+        errorBox.style.display = "block";
+
+        setTimeout(() => {
+            errorBox.style.display = "none";
+        }, 1500);
+        return false;
+    }
+
+    if (password.length < 6 || confirm_pass.length < 6) {
+        errorBox.textContent = "Short Password. (Min 6 characters)";
+        errorBox.style.display = "block";
+
+        setTimeout(() => {
+            errorBox.style.display = "none";
+        }, 1500);
+        return false;
+    }
+
+    if (password != confirm_pass) {
+        errorBox.textContent = "Not the same password!";
+        errorBox.style.display = "block";
+
+        setTimeout(() => {
+            errorBox.style.display = "none";
+        }, 1500);
+        return false;
+    }
+    messageBox.textContent = " Succesful! Redirecting...";
+    messageBox.style.display = "block";
+
+    localStorage.setItem("isLoggedIn", "true");
+    setTimeout(() => {
+        window.location.href = "index.html";
+    }, 1500);
+
+}
+function validateEmail(email) {
+
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
+window.addEventListener("DOMContentLoaded", () => {
+    const loginLink = document.getElementById("login-link");
+    const logoutLink = document.getElementById("logout-link");
+
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+    if (isLoggedIn == "true") {
+        loginLink.style.display = "none";
+        logoutLink.style.display = "inline";
+    }
+    else {
+        loginLink.style.display = "inline";
+        logoutLink.style.display = "none";
+    }
+    if (logoutLink) {
+        logoutLink.addEventListener("click", () => {
+            localStorage.removeItem("isLoggedIn");
+            window.location.href = "login.html";
+        });
+    }
+});
